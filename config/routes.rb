9c4475f88463bb2823fd803de8fed2d39c86e50d
Ertemelle <phenixime@hotmail.fr>
2018-08-02 11:53:57 +0200

    CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/create'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/update'
+  get 'users/destroy'
+	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
