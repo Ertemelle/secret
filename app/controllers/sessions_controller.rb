6ca06a1b152643615d822887174d44827a0e9f81
Léona Chevrel <chevrel.leona@gmail.com>
2018-08-02 18:38:55 +0200

    dynamic header fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      login_url user
-      redirect_to secret_path
+      log_in user
+      redirect_to 'user'
+      puts 'I AM IN'
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to home_path
     puts "je suis plus logerrrr"
   end
